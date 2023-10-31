fa1e8eef9e2f605817b20f71d5298d6b8f1f89b0
Daeeun Ko <7273322@naver.com>
2023-10-31 17:49:13 +0900

    test: 인수 테스트 예시 작성

---
+++ src/test/java/com/nakaligoba/backend/acceptance/AuthAcceptanceTest.java
@@ -0,0 +1,62 @@
+package com.nakaligoba.backend.acceptance;
+
+import io.restassured.http.ContentType;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+
+public class AuthAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    @DisplayName("이메일과 비밀번호와 닉네임으로 회원가입을 할 수 있다.")
+    void givenEmailAndPasswordAndNickName_whenRegister_thenOK() throws JSONException {
+        String email = "test@test.com";
+        String password = "test123";
+        String nickname = "nickname";
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", email);
+        requestBody.put("password", password);
+        requestBody.put("nickname", nickname);
+
+        given()
+                .log().all()
+                .body(requestBody.toString())
+                .contentType(ContentType.JSON)
+        .when()
+                .post("/api/v1/auth/signup")
+        .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.JSON)
+                .body("$", hasKey("message"));
+    }
+
+    @Test
+    @DisplayName("이메일과 비밀번호와 닉네임으로 회원가입을 할 수 있다.")
+    void givenInvalidEmail_whenRegister_thenBadRequest() throws JSONException {
+        String email = "test";
+        String password = "test123";
+        String nickname = "nickname";
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", email);
+        requestBody.put("password", password);
+        requestBody.put("nickname", nickname);
+
+        given()
+                .log().all()
+                .body(requestBody.toString())
+                .contentType(ContentType.JSON)
+        .when()
+                .post("/api/v1/auth/signup")
+        .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+}
