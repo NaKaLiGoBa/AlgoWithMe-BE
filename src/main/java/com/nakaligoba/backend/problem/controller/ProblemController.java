ca608c03752dd71f72c9db38982a507bef5af0d5
Gyujin Kim <kjkj268@naver.com>
2023-11-01 17:07:46 +0900

    springframework.data 패키지 의존 감소 및 response DTO 수정

--- src/main/java/com/nakaligoba/backend/problem/controller/ProblemController.java
+++ src/main/java/com/nakaligoba/backend/problem/controller/ProblemController.java
@@ -5,9 +5,7 @@ import com.nakaligoba.backend.problem.application.dto.ProblemPagingDto;
 import com.nakaligoba.backend.problem.application.ProblemService;
 import com.nakaligoba.backend.problem.controller.dto.ProblemResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,21 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProblemController {
 
     private final ProblemService problemService;
-//    private final ProblemRepository problemRepository;
 
     @GetMapping
-    public CustomPageResponse<ProblemPagingDto> readAllProblems(@PageableDefault(value = 3) Pageable pageable) {
-        Page<ProblemPagingDto> problemPage = problemService.getProblemList(pageable);
-        return new CustomPageResponse<>(problemPage);
+    public ResponseEntity<CustomPageResponse<ProblemPagingDto>> readAllProblems(Pageable pageable) {
+        CustomPageResponse<ProblemPagingDto> response = problemService.getProblemList(pageable);
+        return ResponseEntity.ok(response);
     }
-/*
-    @PostConstruct
-    public void init() {
-        for (int i = 1; i <= 10; i++) {
-            Problem problem = new Problem((long) i, "default description","Problem " + i,"쉬움", new BigDecimal("66.6"));
-            problemRepository.save(problem);
-        }
-    }*/
 
     @GetMapping("/{id}")
     public ResponseEntity<ProblemResponse> readProblem(@PathVariable Long id) {
