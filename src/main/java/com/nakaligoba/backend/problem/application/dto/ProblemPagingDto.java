c822f155cf1906974405b3b28ef38f1052f67d19
Gyujin Kim <kjkj268@naver.com>
2023-11-01 17:07:46 +0900

    springframework.data 패키지 의존 감소 및 response DTO 수정

--- src/main/java/com/nakaligoba/backend/problem/application/dto/ProblemPagingDto.java
+++ src/main/java/com/nakaligoba/backend/problem/application/dto/ProblemPagingDto.java
@@ -23,15 +23,4 @@ public class ProblemPagingDto {
     private BigDecimal acceptance;
     private String difficulty;
     private List<String> tags;
-
-    // Todo. 정답률, status 및 tags 수정하기
-    public ProblemPagingDto(Problem problem) {
-        this.id = problem.getId();
-        this.number = problem.getNumber();
-        this.status = "성공";
-        this.title = problem.getTitle();
-        this.acceptance = problem.getAcceptance();
-        this.difficulty = problem.getDifficulty();
-        this.tags = Arrays.asList("DFS", "BFS");
-    }
 }
