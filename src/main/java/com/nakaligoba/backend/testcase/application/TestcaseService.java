2c3f46ae6789e6239d09abf680a04b6da41ba15a
Daeeun Ko <7273322@naver.com>
2023-11-02 16:11:20 +0900

    feat: 테스트케이스 생성 기능 추가

---
+++ src/main/java/com/nakaligoba/backend/testcase/application/TestcaseService.java
@@ -0,0 +1,51 @@
+package com.nakaligoba.backend.testcase.application;
+
+import com.nakaligoba.backend.problem.domain.Problem;
+import com.nakaligoba.backend.testcase.domain.Testcase;
+import com.nakaligoba.backend.testcase.domain.TestcaseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class TestcaseService {
+
+    private static final String INPUT_DELIMITER = " ";
+    private final static Integer ANSWER_CASE_NUMBER = 1;
+
+    private final TestcaseRepository testcaseRepository;
+
+    @Transactional
+    public Testcase createTestcase(Integer number, List<String> parameters, String inputs, String output, Problem problem) {
+        String inputNames = String.join(INPUT_DELIMITER, parameters);
+        Testcase testcase = Testcase.builder()
+                .number(number)
+                .inputNames(inputNames)
+                .inputValues(inputs)
+                .output(output)
+                .isGrading(false)
+                .problem(problem)
+                .build();
+        return testcaseRepository.save(testcase);
+    }
+
+    @Transactional
+    public Testcase createAnswerCase(List<String> parameters, String inputs, String output, Problem problem) {
+        String inputNames = String.join(INPUT_DELIMITER, parameters);
+        Testcase answerCase = Testcase.builder()
+                .number(ANSWER_CASE_NUMBER)
+                .inputNames(inputNames)
+                .inputValues(inputs)
+                .output(output)
+                .isGrading(false)
+                .problem(problem)
+                .build();
+        return testcaseRepository.save(answerCase);
+    }
+
+
+}
