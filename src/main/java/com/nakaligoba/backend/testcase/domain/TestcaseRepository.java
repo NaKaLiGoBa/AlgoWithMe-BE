81cb463dca33acbf60d3ca4fb8692523c6189a46
Daeeun Ko <7273322@naver.com>
2023-11-02 16:10:45 +0900

    feat: 태그 생성 기능 추가

---
+++ src/main/java/com/nakaligoba/backend/testcase/domain/TestcaseRepository.java
@@ -0,0 +1,6 @@
+package com.nakaligoba.backend.testcase.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestcaseRepository extends JpaRepository<Testcase, Long> {
+}
