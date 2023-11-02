435cac0cb972a39e9ba1fdd0cb6b967ebdceec65
Seokjon Son <tnh3113@naver.com>
2023-11-02 14:52:56 +0900

    chore: 리뷰 부분 수정
     - SolutionCreateRequest 내 검증을 위한 어노테이션 추가
     - SolutionService 내 createSolution() 메서드에 이메일 전달하여 처리
     - 전달된 풀이 글 작성 언어의 개수만큼 save하는 것이 아닌 arrayList를 통해 한 번에 saveAll하도록 수정

--- src/main/java/com/nakaligoba/backend/solution/controller/dto/SolutionCreateRequest.java
+++ src/main/java/com/nakaligoba/backend/solution/controller/dto/SolutionCreateRequest.java
@@ -2,11 +2,18 @@ package com.nakaligoba.backend.solution.controller.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 
 @Data
 public class SolutionCreateRequest {
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String content;
+
+    @NotNull
     private ArrayList<String> languages;
 }
