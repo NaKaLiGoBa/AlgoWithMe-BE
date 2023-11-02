0927b6d63eb9007b12857a827583bad2d0e27f31
Daeeun Ko <7273322@naver.com>
2023-11-02 16:12:38 +0900

    feat: 문제에 풀이 가능한 언어 추가 기능 추가

---
+++ src/main/java/com/nakaligoba/backend/availablelanguage/domain/AvailableLanguageRepository.java
@@ -0,0 +1,6 @@
+package com.nakaligoba.backend.availablelanguage.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvailableLanguageRepository extends JpaRepository<AvailableLanguage, Long> {
+}
