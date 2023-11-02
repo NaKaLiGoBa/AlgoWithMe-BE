62b58f17c2ca993492c53c51ac722ae4017ded03
Daeeun Ko <7273322@naver.com>
2023-11-02 16:12:11 +0900

    feat: 문제에 태그 추가 기능 추가

---
+++ src/main/java/com/nakaligoba/backend/problemtag/domain/ProblemTagRepository.java
@@ -0,0 +1,6 @@
+package com.nakaligoba.backend.problemtag.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProblemTagRepository extends JpaRepository<ProblemTag, Long> {
+}
