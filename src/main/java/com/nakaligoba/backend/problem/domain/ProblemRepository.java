98fd784396de6219fe6121263b2039b314abd4ac
Daeeun Ko <7273322@naver.com>
2023-11-02 16:13:32 +0900

    feat: 가장 마지막 문제 번호 찾기 기능 추가

--- src/main/java/com/nakaligoba/backend/problem/domain/ProblemRepository.java
+++ src/main/java/com/nakaligoba/backend/problem/domain/ProblemRepository.java
@@ -1,6 +1,12 @@
 package com.nakaligoba.backend.problem.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
+
+    @Query("select max(p.number) from Problem p")
+    Optional<Integer> findMaxNumber();
 }
