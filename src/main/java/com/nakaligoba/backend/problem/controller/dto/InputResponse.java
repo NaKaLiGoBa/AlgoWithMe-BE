5e4ca4ce550fa2e746a5a60d62723aca589e9014
Daeeun Ko <7273322@naver.com>
2023-11-06 05:45:39 +0900

    test: 테스트케이스 확인 인수테스트 작성

---
+++ src/main/java/com/nakaligoba/backend/problem/controller/dto/InputResponse.java
@@ -0,0 +1,9 @@
+package com.nakaligoba.backend.problem.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class InputResponse {
+    private final String name;
+    private final String value;
+}
---
+++ src/test/java/com/nakaligoba/backend/acceptance/ProblemCodeAcceptanceTest.java
@@ -0,0 +1,36 @@
+package com.nakaligoba.backend.acceptance;
+
+import com.nakaligoba.backend.acceptance.fixtures.ProblemFixture;
+import com.nakaligoba.backend.problem.controller.dto.CheckTestcaseRequest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import static io.restassured.RestAssured.given;
+
+public class ProblemCodeAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    @DisplayName("문제 식별자, 프로그래밍 언어, 코드로 해당 문제의 테스트케이스들의 성공 실패 여부를 알 수 있다.")
+    void testcaseCheck() {
+        String location = ProblemFixture.createProblem()
+                .response()
+                .getHeader(HttpHeaders.LOCATION);
+        String language = "Java";
+        String code = "class Solution {\npublic String solve(String a, String b) {\n        return String.valueOf(Integer.parseInt(a) + Integer.parseInt(b));\n    }\n}";
+
+        CheckTestcaseRequest requestBody = new CheckTestcaseRequest(language, code);
+
+        given()
+                .log().all()
+                .body(requestBody)
+                .contentType(ContentType.JSON)
+        .when()
+                .post(location + "/code/test")
+        .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
