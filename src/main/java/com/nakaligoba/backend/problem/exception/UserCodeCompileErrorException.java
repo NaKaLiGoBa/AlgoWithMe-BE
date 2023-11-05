cab9d89b95470910649b2ce1fb0bd124eda3c2df
Daeeun Ko <7273322@naver.com>
2023-11-06 08:50:47 +0900

    feat: 사용자 코드 예외 추가

---
+++ src/main/java/com/nakaligoba/backend/problem/exception/UserCodeCompileErrorException.java
@@ -0,0 +1,8 @@
+package com.nakaligoba.backend.problem.exception;
+
+public class UserCodeCompileErrorException extends UserCodeErrorException {
+
+    public UserCodeCompileErrorException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/nakaligoba/backend/problem/exception/UserCodeRuntimeErrorException.java
@@ -0,0 +1,8 @@
+package com.nakaligoba.backend.problem.exception;
+
+public class UserCodeRuntimeErrorException extends UserCodeErrorException {
+
+    public UserCodeRuntimeErrorException(String message) {
+        super(message);
+    }
+}
