65c245db9ff27e65757c92a256443a648a0eb18d
Seokjon Son <tnh3113@naver.com>
2023-11-05 12:13:35 +0900

    feat: 풀이 글 리스트 보기(#31)

--- src/main/java/com/nakaligoba/backend/solution/domain/Solution.java
+++ src/main/java/com/nakaligoba/backend/solution/domain/Solution.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class Solution extends BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "title", nullable = false)
