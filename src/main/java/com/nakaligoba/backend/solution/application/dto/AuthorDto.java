21f698233792d2da982bc6b6534fb36ed6fcda86
Seokjon Son <tnh3113@naver.com>
2023-11-05 12:13:35 +0900

    feat: 풀이 글 리스트 보기(#31)

---
+++ src/main/java/com/nakaligoba/backend/solution/application/dto/AuthorDto.java
@@ -0,0 +1,15 @@
+package com.nakaligoba.backend.solution.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorDto {
+    private String avatar;
+    private String nickname;
+}
