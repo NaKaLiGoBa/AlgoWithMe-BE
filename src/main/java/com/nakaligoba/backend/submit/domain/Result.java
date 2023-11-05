9c88e8e9dc4ca7499aa68ceeb097a57afd8b0ac8
Daeeun Ko <7273322@naver.com>
2023-11-06 08:50:27 +0900

    feat: 제출 결과에 런타임 오류 추가

--- src/main/java/com/nakaligoba/backend/submit/domain/Result.java
+++ src/main/java/com/nakaligoba/backend/submit/domain/Result.java
@@ -1,5 +1,9 @@
 package com.nakaligoba.backend.submit.domain;
 
 public enum Result {
-    RESOLVED, FAIL, COMPILE_ERROR, UN_RESOLVED
+    RESOLVED, FAIL, COMPILE_ERROR, RUNTIME_ERROR, UN_RESOLVED;
+
+    public static Result isResolved(boolean isAnswer) {
+        return isAnswer ? Result.RESOLVED : Result.UN_RESOLVED;
+    }
 }
