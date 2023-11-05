643c0ea4e0145115907d85b18d649874470080e5
Daeeun Ko <7273322@naver.com>
2023-11-06 06:03:52 +0900

    chore: 테스트케이스 통과 여부 확인 요청 바디 DTO 이름 변경

--- src/main/java/com/nakaligoba/backend/problem/controller/dto/CheckCodeRequest.java
+++ src/main/java/com/nakaligoba/backend/problem/controller/dto/CheckCodeRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import javax.validation.constraints.NotBlank;
 
 @Data
-public class CheckTestcaseRequest {
+public class CheckCodeRequest {
     @NotBlank
     private final String language;
     @NotBlank
