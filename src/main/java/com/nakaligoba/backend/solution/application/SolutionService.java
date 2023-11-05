74477d35542595614d88d0183c66bf8b019eb6e4
Seokjon Son <tnh3113@naver.com>
2023-11-05 12:13:35 +0900

    feat: 풀이 글 리스트 보기(#31)

--- src/main/java/com/nakaligoba/backend/solution/application/SolutionService.java
+++ src/main/java/com/nakaligoba/backend/solution/application/SolutionService.java
@@ -8,15 +8,25 @@ import com.nakaligoba.backend.problem.domain.ProblemRepository;
 import com.nakaligoba.backend.programminglanguage.domain.ProgrammingLanguage;
 import com.nakaligoba.backend.programminglanguage.domain.ProgrammingLanguageRepository;
 import com.nakaligoba.backend.solution.controller.dto.SolutionRequest;
+import com.nakaligoba.backend.solution.application.dto.AuthorDto;
+import com.nakaligoba.backend.solution.controller.dto.SolutionsResponse;
+import com.nakaligoba.backend.solution.controller.dto.SolutionsResponse.Link;
+import com.nakaligoba.backend.solution.controller.dto.SolutionsResponse.Solutions;
+import com.nakaligoba.backend.solution.controller.dto.SolutionsResponse.SolutionsData;
 import com.nakaligoba.backend.solution.domain.Solution;
 import com.nakaligoba.backend.solution.domain.SolutionRepository;
 import com.nakaligoba.backend.solutionlanguage.domain.SolutionLanguage;
 import com.nakaligoba.backend.solutionlanguage.domain.SolutionLanguageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
+
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,6 +40,7 @@ public class SolutionService {
     private final ProgrammingLanguageRepository programmingLanguageRepository;
     private final SolutionLanguageRepository solutionLanguageRepository;
 
+    @Transactional
     public long createSolution(String writerEmail, long problemId, SolutionRequest request) {
         Member member = memberRepository.findByEmail(writerEmail);
         Problem problem = problemRepository.findById(problemId)
@@ -102,5 +113,51 @@ public class SolutionService {
         }
 
         solutionRepository.delete(solution);
+    }
+
+    @Transactional
+    public SolutionsResponse readSolutions(long problemId, long nextCursorId, int size) {
+        long solutionTotalCount = solutionRepository.countByProblemId(problemId);
+        List<Solution> solutions = getSolutions(problemId, nextCursorId, size);
+        List<Solutions> responseSolutions = getResponseSolutions(solutions);
+        long nextCursor = responseSolutions.size() != 0 ?
+                responseSolutions.get(responseSolutions.size() - 1).getSolution().getId() : -1;
+
+        return SolutionsResponse.builder()
+                .totalCount(solutionTotalCount)
+                .solutions(responseSolutions)
+                ._link(Link.builder()
+                        .nextCursor(nextCursor)
+                        .build())
+                .build();
+    }
+
+    private List<Solution> getSolutions(long problemId, long nextCursorId, int size) {
+        Pageable pageable = PageRequest.of(0, size);
+
+        if (nextCursorId == SolutionsResponse.READ_SOLUTIONS_INIT) {
+            return solutionRepository.findByProblemIdOrderByCreatedAtDesc(problemId, pageable);
+        }
+
+        if (nextCursorId > SolutionsResponse.READ_SOLUTIONS_EMPTY) {
+            return solutionRepository.findByProblemIdAndIdLessThanOrderByCreatedAtDesc(problemId, nextCursorId, pageable);
+        }
+
+        return Collections.emptyList();
+    }
+
+    private List<Solutions> getResponseSolutions(List<Solution> solutions) {
+        return solutions.stream().map(solution ->
+                        Solutions.builder()
+                                .author(AuthorDto.builder()
+                                        .avatar("")
+                                        .nickname(solution.getMember().getNickname())
+                                        .build())
+                                .solution(SolutionsData.builder()
+                                        .id(solution.getId())
+                                        .title(solution.getTitle())
+                                        .build())
+                                .build())
+                .collect(Collectors.toList());
     }
 }
