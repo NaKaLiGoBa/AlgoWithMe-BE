a5188c6d2115c44690fac7b953a02ee437c828b5
Daeeun Ko <7273322@naver.com>
2023-11-06 14:27:08 +0900

    fix: 문제 실패 여부 수정

--- src/main/java/com/nakaligoba/backend/domain/Result.java
+++ src/main/java/com/nakaligoba/backend/domain/Result.java
@@ -4,6 +4,6 @@ public enum Result {
     RESOLVED, FAIL, COMPILE_ERROR, RUNTIME_ERROR, UN_RESOLVED;
 
     public static Result isResolved(boolean isAnswer) {
-        return isAnswer ? Result.RESOLVED : Result.UN_RESOLVED;
+        return isAnswer ? Result.RESOLVED : Result.FAIL;
     }
 }
