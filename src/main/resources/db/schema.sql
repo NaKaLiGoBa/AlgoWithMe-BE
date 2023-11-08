499169bd9bc5d50bf011980d1fe97b52f2d143f0
김규진 <67509973+gyujin-Kimm@users.noreply.github.com>
2023-11-08 14:13:08 +0900

    스키마 수정

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -37,6 +37,7 @@ CREATE TABLE `members`
     `email`      VARCHAR(255) NOT NULL,
     `password`   VARCHAR(255) NOT NULL,
     `nickname`   VARCHAR(255) NOT NULL,
+    `role`       VARCHAR(255) NOT NULL,
     `created_at` DATETIME     NOT NULL,
     `updated_at` DATETIME     NOT NULL,
     PRIMARY KEY (`id`)
