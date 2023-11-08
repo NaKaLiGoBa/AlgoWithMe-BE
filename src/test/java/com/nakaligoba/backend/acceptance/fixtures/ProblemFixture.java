cca3b3556c8e6d1650f65ca7ec3ffcf3cc7416ae
Daeeun Ko <7273322@naver.com>
2023-11-08 17:03:43 +0900

    test: 문제 Fixture 문제 생성 DTO 상수화

--- src/test/java/com/nakaligoba/backend/acceptance/fixtures/ProblemFixture.java
+++ src/test/java/com/nakaligoba/backend/acceptance/fixtures/ProblemFixture.java
@@ -11,20 +11,18 @@ import static io.restassured.RestAssured.given;
 
 public class ProblemFixture {
 
-    public static ExtractableResponse<Response> createProblem() {
-        CreateProblemRequest requestBody = new CreateProblemRequest(
-                "겁나 어려운 문제",
-                "어려움",
-                "# 설명\n매우 어려운 설명\n## 소제목",
-                Arrays.asList("a", "b"),
-                "1 2\n3",
-                "3 4\n7",
-                Arrays.asList("DFS", "BFS")
-        );
+    public final static CreateProblemRequest CREATE_PROBLEM_REQUEST = new CreateProblemRequest("겁나 어려운 문제",
+            "어려움",
+            "# 설명\n매우 어려운 설명\n## 소제목",
+            Arrays.asList("a", "b"),
+            "1 2\n3",
+            "3 4\n7",
+            Arrays.asList("DFS", "BFS"));
 
+    public static ExtractableResponse<Response> createProblem() {
         return given()
                 .log().all()
-                .body(requestBody)
+                .body(CREATE_PROBLEM_REQUEST)
                 .contentType(ContentType.JSON)
                 .when()
                 .post("/api/v1/problems")
