7868fff4eab49e2b26a4f7bb7c7fd0cf559cc451
Gyujin Kim <kjkj268@naver.com>
2023-11-11 03:51:22 +0900

    feat: 답글 변경 시 작성자가 아니면 FORBIDDEN 예외 추가

---
+++ src/main/java/com/nakaligoba/backend/exception/UnauthorizedAccessException.java
@@ -0,0 +1,8 @@
+package com.nakaligoba.backend.exception;
+
+public class UnauthorizedAccessException extends RuntimeException {
+
+    public UnauthorizedAccessException(String message) {
+        super(message);
+    }
+}
