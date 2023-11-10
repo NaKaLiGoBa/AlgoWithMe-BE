35615c2bf90d34a756803fd6599fdc65a425dae7
Gyujin Kim <kjkj268@naver.com>
2023-11-11 03:47:37 +0900

    feat: 답글 수정

--- src/main/java/com/nakaligoba/backend/service/impl/ReplyService.java
+++ src/main/java/com/nakaligoba/backend/service/impl/ReplyService.java
@@ -1,9 +1,11 @@
 package com.nakaligoba.backend.service.impl;
 
+import com.github.dockerjava.api.exception.UnauthorizedException;
 import com.nakaligoba.backend.controller.payload.request.ReplyRequest;
 import com.nakaligoba.backend.domain.Comment;
 import com.nakaligoba.backend.domain.Member;
 import com.nakaligoba.backend.domain.Reply;
+import com.nakaligoba.backend.exception.UnauthorizedAccessException;
 import com.nakaligoba.backend.repository.CommentRepository;
 import com.nakaligoba.backend.repository.ReplyRepository;
 import lombok.RequiredArgsConstructor;
@@ -35,5 +37,21 @@ public class ReplyService {
         replyRepository.save(reply);
 
         return reply.getId();
+    }
+
+    @Transactional
+    public void updateReply(String writerEmail, Long commendId, Long replyId, ReplyRequest request) {
+        Member member = memberService.findByEmail(writerEmail)
+                .orElseThrow(EntityNotFoundException::new);
+        Comment comment = commentRepository.findById(commendId)
+                .orElseThrow(EntityNotFoundException::new);
+        Reply reply = replyRepository.findById(replyId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        if (!reply.getMember().equals(member)) {
+            throw new UnauthorizedAccessException("권한이 없습니다.");
+        }
+
+        reply.changeReplyContent(request.getContent());
     }
 }
