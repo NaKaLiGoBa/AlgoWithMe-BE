ed71a52c79f158605d3845c9e32e6586cc658328
Daeeun Ko <7273322@naver.com>
2023-11-13 05:27:03 +0900

    feat: 풀이 글 좋아요 기능 추가

--- src/main/java/com/nakaligoba/backend/repository/SolutionLikeRepository.java
+++ src/main/java/com/nakaligoba/backend/repository/SolutionLikeRepository.java
@@ -1,10 +1,16 @@
 package com.nakaligoba.backend.repository;
 
+import com.nakaligoba.backend.domain.Member;
+import com.nakaligoba.backend.domain.Solution;
 import com.nakaligoba.backend.domain.SolutionLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface SolutionLikeRepository extends JpaRepository<SolutionLike, Long> {
     Long countBySolutionId(Long solutionId);
 
     boolean existsByMemberIdAndSolutionId(Long memberId, Long solutionId);
+
+    Optional<SolutionLike> findByMemberAndSolution(Member member, Solution solution);
 }
