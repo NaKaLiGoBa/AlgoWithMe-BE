b5c9dd9c3f42e961f93558215abf927efa18dae3
Seokjon Son <tnh3113@naver.com>
2023-11-17 13:17:35 +0900

    feat: 풀이 삭제 시 풀이 조회 테이블도 삭제

--- src/main/java/com/nakaligoba/backend/repository/SolutionViewRepository.java
+++ src/main/java/com/nakaligoba/backend/repository/SolutionViewRepository.java
@@ -1,8 +1,13 @@
 package com.nakaligoba.backend.repository;
 
+import com.nakaligoba.backend.domain.Solution;
 import com.nakaligoba.backend.domain.SolutionView;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SolutionViewRepository extends JpaRepository<SolutionView, Long> {
-    Long countBySolutionId(Long solutionId);
+    Optional<Long> countBySolutionId(Long solutionId);
+
+    void deleteBySolutionId(Long solutionId);
 }
--- src/main/java/com/nakaligoba/backend/service/impl/SolutionViewService.java
+++ src/main/java/com/nakaligoba/backend/service/impl/SolutionViewService.java
@@ -1,11 +1,11 @@
 package com.nakaligoba.backend.service.impl;
 
+import com.nakaligoba.backend.domain.Solution;
 import com.nakaligoba.backend.domain.SolutionView;
 import com.nakaligoba.backend.repository.SolutionViewRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -15,10 +15,14 @@ public class SolutionViewService {
     private final SolutionViewRepository solutionViewRepository;
 
     public Long countBySolutionId(Long solutionId) {
-        return solutionViewRepository.countBySolutionId(solutionId);
+        return solutionViewRepository.countBySolutionId(solutionId).orElse(0L);
     }
 
     public void createSolutionView(SolutionView solutionView) {
         solutionViewRepository.save(solutionView);
+    }
+
+    public void deleteBySolutionId(Long solutionId) {
+        solutionViewRepository.deleteBySolutionId(solutionId);
     }
 }
