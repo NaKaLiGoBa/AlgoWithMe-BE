7cbf69273f7883ea4027776903d8bb5d801cbf3c
Daeeun Ko <7273322@naver.com>
2023-11-20 17:00:26 +0900

    hotfix: RestTemplate 프록시 설정

--- src/main/java/com/nakaligoba/backend/global/config/WebConfig.java
+++ src/main/java/com/nakaligoba/backend/global/config/WebConfig.java
@@ -1,20 +1,20 @@
 package com.nakaligoba.backend.global.config;
 
+import org.apache.http.HttpHost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 
 @Configuration
 public class WebConfig {
     @Bean
     public RestTemplate restTemplate() {
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("krmp-proxy.9rum.cc", 3128));
-        requestFactory.setProxy(proxy);
-        return new RestTemplate(requestFactory);
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setProxy(new HttpHost("krmp-proxy.9rum.cc", 3128))
+                .build();
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
     }
 }
