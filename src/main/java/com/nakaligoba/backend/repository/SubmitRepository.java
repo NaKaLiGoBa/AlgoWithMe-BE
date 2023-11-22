e0a5813c2e9c3ec8c819baff03ed12f3ec93eec3
Daeeun Ko <7273322@naver.com>
2023-11-22 18:10:41 +0900

    feat: 제출 목록 조회 API 생성

--- src/main/java/com/nakaligoba/backend/repository/SubmitRepository.java
+++ src/main/java/com/nakaligoba/backend/repository/SubmitRepository.java
@@ -1,5 +1,6 @@
 package com.nakaligoba.backend.repository;
 
+import com.nakaligoba.backend.domain.Member;
 import com.nakaligoba.backend.domain.Problem;
 import com.nakaligoba.backend.domain.Submit;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ import java.util.Optional;
 
 public interface SubmitRepository extends JpaRepository<Submit, Long> {
     List<Submit> findAllByProblem(Problem problem);
+
+    List<Submit> findAllByMemberAndProblem(Member member, Problem problem);
 }
