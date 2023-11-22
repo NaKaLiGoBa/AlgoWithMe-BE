35914e1244690a320ef621d149fd398ef360666c
Gyujin Kim <kjkj268@naver.com>
2023-11-22 11:07:35 +0900

    fix: Problem 목록조회 카테고라이징 현재작업까지.

--- src/main/java/com/nakaligoba/backend/domain/Result.java
+++ src/main/java/com/nakaligoba/backend/domain/Result.java
@@ -1,9 +1,33 @@
 package com.nakaligoba.backend.domain;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public enum Result {
-    RESOLVED, FAIL, COMPILE_ERROR, RUNTIME_ERROR, UN_RESOLVED;
+    RESOLVED("성공"),
+    FAIL("실패"),
+    COMPILE_ERROR("컴파일 에러"),
+    RUNTIME_ERROR("런타임 에러"),
+    UN_RESOLVED("미해결");
+
+    private final String korean;
 
     public static Result isResolved(boolean isAnswer) {
         return isAnswer ? Result.RESOLVED : Result.FAIL;
+    }
+
+    Result(String korean) {
+        this.korean = korean;
+    }
+
+    public static Result getByKorean(String korean) {
+        return Arrays.stream(Result.values())
+                .filter(d -> d.korean.equals(korean))
+                .findAny()
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    public String getKorean() {
+        return korean;
     }
 }
--- src/main/java/com/nakaligoba/backend/repository/ProblemRepository.java
+++ src/main/java/com/nakaligoba/backend/repository/ProblemRepository.java
@@ -1,14 +1,8 @@
 package com.nakaligoba.backend.repository;
 
-import com.nakaligoba.backend.domain.Difficulty;
 import com.nakaligoba.backend.domain.Problem;
-import com.nakaligoba.backend.domain.Result;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,13 +11,4 @@ public interface ProblemRepository extends JpaRepository<Problem, Long> {
 
     @Query("select max(p.number) from Problem p")
     Optional<Integer> findMaxNumber();
-
-    @Query("SELECT p FROM Problem p LEFT JOIN p.submits s LEFT JOIN p.problemTags pt LEFT JOIN pt.tag t " +
-            "WHERE (:status IS NULL OR s.result = :status) " +
-            "AND (:difficulty IS NULL OR p.difficulty = :difficulty) " +
-            "AND (:tags IS EMPTY OR t.name IN :tags)")
-    Page<Problem> findAllWithFilters(Pageable pageable,
-                                     @Param("status") Result status,
-                                     @Param("difficulty") Difficulty difficulty,
-                                     @Param("tags") List<String> tags);
 }
