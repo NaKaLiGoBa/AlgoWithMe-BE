ed853aaa44c462ceba830f996a5599dc572a6790
Daeeun Ko <7273322@naver.com>
2023-11-23 11:39:41 +0900

    hotfix: 좋아요 요청 request body 기본 생성자 추가

--- src/main/java/com/nakaligoba/backend/controller/payload/request/LikeRequest.java
+++ src/main/java/com/nakaligoba/backend/controller/payload/request/LikeRequest.java
@@ -1,8 +1,12 @@
 package com.nakaligoba.backend.controller.payload.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LikeRequest {
-    private final String requestDateTime;
+    private String requestDateTime;
 }
