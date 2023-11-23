854e1f9606fc9979bcef97c6afd3c0bbe1bb8dfd
Seokjon Son <tnh3113@naver.com>
2023-11-23 15:43:01 +0900

    충돌난 부분 수정

--- src/main/java/com/nakaligoba/backend/service/impl/SubmitService.java
+++ src/main/java/com/nakaligoba/backend/service/impl/SubmitService.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityNotFoundException;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
