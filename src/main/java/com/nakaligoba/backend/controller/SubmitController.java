9f777abe7985a84813e5f3b58c07badbd1ac6095
Seokjon Son <tnh3113@naver.com>
2023-11-24 12:41:53 +0900

    hoxfix: SubmitController 내 RestController 추가

--- src/main/java/com/nakaligoba/backend/controller/SubmitController.java
+++ src/main/java/com/nakaligoba/backend/controller/SubmitController.java
@@ -11,12 +11,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
 @Slf4j
 @RequestMapping("/api/v1/problems/{problemId}/submits")
 @RequiredArgsConstructor
+@RestController
 public class SubmitController {
 
     private final SubmitService submitService;
